4ce98b1cd1005d440305a481fdd31d4d415159c0
Scott Savage <snsavage@gmail.com>
2016-09-30 17:08:51 -0400

    Create RailsCasts with basic functionality
    At this point RailsCasts has the ability to take a url and return an
    array of Episodes.  To complete this functionality an Episodes and a
    NullNokogiri class were added.
    
    Episodes is a class to handle the data structure for an individual
    episode.
    
    NullNokogiri uses the Null Object Pattern to handle cases were Nokogiri
    returns no results.
    
    Specs are provided for each of the methods in RailsCasts.  In this
    commit all methods are private, so the specs utilize "send" to send
    messages to the private methods.  This should be changed so that the
    testing using the publicly available attr_readers.

--- lib/retro_casts/rails_casts.rb
+++ lib/retro_casts/rails_casts.rb
@@ -1,21 +1,64 @@
 module RetroCasts
+  class Episode
+    attr_accessor :title, :number, :date, :length, :link, :description
+  end
+
+  class NullNokogiri
+    def empty?
+      true
+    end
+
+    def css(*)
+      []
+    end
+  end
+
   class RailsCasts
     HOST = 'http://www.railscasts.com/'
+    NODE_SET_FILTER = '.episode'
 
-    def self.get_page(url)
+    attr_reader :url, :page, :nodeset, :episodes
+
+    def initialize(url)
+      @url = url
+      @page = get_page
+      @nodeset = get_nodeset(page: page, filter: NODE_SET_FILTER)
+      @episodes = []
+    end
+
+    private
+    def get_page
       begin
         Nokogiri::HTML(open(url))
       rescue
-        nil
+        RetroCasts::NullNokogiri.new
       end
     end
 
-    # def self.get_nodeset(page, filter)
-    #   page.css(filter)
-    # end
-  end
+    def get_nodeset(page: self.page, filter: NODE_SET_FILTER)
+      nodeset = page.css(filter)
+      if nodeset.empty?
+        NullNokogiri.new
+      else
+        nodeset
+      end
+    end
 
-  class NullDocument
+    def parse_episodes
+      @episdoes = nodeset.collect do |node|
+        create_episode(node)
+      end
+    end
+
+    def create_episode(node)
+      episode = Episode.new
+      episode.title = node.css(".main h2 a").text
+      episode.number = node.css(".number").text
+      episode.date = node.css(".pulished_at").text
+      episode.length = node.css(".stats").text
+      episode.link = node.css(".screenshot a").first.attributes["href"].value
+      return episode
+    end
   end
 end
 
--- spec/rails_casts_spec.rb
+++ spec/rails_casts_spec.rb
@@ -1,35 +1,130 @@
 require 'spec_helper'
 
-describe 'RailsCasts' do
-  it 'has a HOST' do
-    expect(RetroCasts::RailsCasts::HOST).to eq('http://www.railscasts.com/')
+describe RetroCasts::RailsCasts do
+  before (:each) do
+    @klass = RetroCasts::RailsCasts
+    @episode_klass = RetroCasts::Episode
+    @url = 'http://www.railscasts.com/'
   end
 
-  describe '.get_page' do
-    context 'when provided with a valid url'  do
+  it 'has a HOST' do
+    expect(@klass::HOST).to eq(@url)
+  end
+
+  it 'has a NODE_SET_FILTER' do
+    expect(@klass::NODE_SET_FILTER).to eq('.episode')
+  end
+
+  describe '#get_page' do
+    let(:site) {@klass.new('http://www.railscasts.com')}
+
+    context 'with a valid url'  do
       it 'returns a nokogiri html document' do
-        page = RetroCasts::RailsCasts.get_page('http://www.railscasts.com')
+        page = site.send(:get_page)
         expect(page).to be_a(Nokogiri::HTML::Document)
       end
     end
 
-    context 'when provided with an invalid url' do
-      it 'returns nil' do
-        page = RetroCasts::RailsCasts.get_page('')
-        expect(page).to be(nil)
+    context 'with an invalid url' do
+      it 'returns NullNokogiri' do
+        site = @klass.new('')
+        page = site.send(:get_page)
+        expect(page).to be_a(RetroCasts::NullNokogiri)
       end
     end
   end
 
-  describe '.get_list' do
-    context 'when given a valid css reference' do
+  describe '#get_nodeset' do
+    context 'with a valid css class' do
       it 'returns a nokogiri xml nodeset' do
-        url = RetroCasts::RailsCasts::HOST
-        page = RetroCasts::RailsCasts.get_page(url)
-        filter = ".episode"
-        nodeset = RetroCasts::RailsCasts.get_nodeset(page, filter)
-        expect(page).to be_a(Nokogiri::XML::NodeSet)
+        site = @klass.new(@url)
+        nodeset = site.send(:get_nodeset)
+        expect(nodeset).to be_a(Nokogiri::XML::NodeSet)
+      end
+    end
+
+    context 'with an invalid css class' do
+      it 'returns NullNokogiri' do
+        site = @klass.new(@url)
+        nodeset = site.send(:get_nodeset, filter: ".invalid-class")
+        expect(nodeset).to be_a(RetroCasts::NullNokogiri)
       end
     end
   end
+
+  describe '#new' do
+    let(:site) do
+      @klass.new(@url)
+    end
+
+    it 'accepts one argument' do
+      expect(@klass).to receive(:new).with(anything)
+      @klass.new(@url)
+    end
+
+    it 'sets a url attribute' do
+      expect(site.url).to eq(@url)
+    end
+
+    it 'sets a page attribute to a Nokogiri::HTML::Document' do
+      expect(site.page).to be_a(Nokogiri::HTML::Document)
+    end
+
+    it 'sets a nodeset to a Nokogiri::XML::NodeSet' do
+      expect(site.nodeset).to be_a(Nokogiri::XML::NodeSet)
+    end
+
+    it 'returns an array of Episodes' do
+      expect(site.send(:parse_episodes)).to all(be_a(@episode_klass))
+    end
+  end
+
+  describe '#create_episode' do
+    let(:site) do
+      @klass.new(@url)
+    end
+
+    it 'takes a html episode as an argument and returns a new episode' do
+      episode = site.nodeset.first
+      expect(site.send(:create_episode, episode)).to be_a(RetroCasts::Episode)
+    end
+  end
+
+  describe '#parse_episodes' do
+    let(:site) {@klass.new(@url)}
+
+    it 'returns an array ' do
+      expect(site.send(:parse_episodes)).to be_a(Array)
+    end
+
+    it 'returns an array of episodes' do
+      expect(site.send(:parse_episodes)).to all(be_a(@episode_klass))
+    end
+  end
+
+  describe '#episodes' do
+    let(:site) {site = RetroCasts::RailsCasts.new('http://www.railscasts.com')}
+
+    it 'returns an array' do
+      expect(site.episodes).to be_a(Array)
+    end
+
+    it 'returns an array with an instance of Episode' do
+      expect(site.episodes).to all(be_an_instance_of(RetroCasts::Episode))
+    end
+  end
+
+  # describe 'Episode' do
+  #   describe 'episode attribute getters' do
+  #     let(:episode) {RetroCasts::RailsCasts.new('http://www.railscasts.com')}
+
+  #     it {expect(episode.title).to eq("Foundation")}
+  #     it {expect(episode.number).to eq("Episode #417")}
+  #     it {expect(episode.date).to eq("Jun 16, 2013")}
+  #     it {expect(episode.length).to eq("(11 minutes)")}
+  #     it {expect(episode.link).to eq("episode/417-foundation")}
+  #     it {expect(episode.description).to include("ZURB's Foundation is a front-end for quickly building applications and prototypes. It is similar to Twitter Bootstrap but uses Sass instead of LESS. Here you will learn the basics of the grid system, navigation, tooltips and more.")}
+  #   end
+  # end
 end
+
